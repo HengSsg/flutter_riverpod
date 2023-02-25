c289f2555f9b84b8a34a5d8b6fa81eaa91807449
HengSsg <dnflekf2748@gmail.com>
2023-02-26 02:05:05 +0900

    JSON 데이터 매핑하기

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -1,5 +1,6 @@
 import 'package:actual/common/const/data.dart';
 import 'package:actual/restaurant/component/restaurant_card.dart';
+import 'package:actual/restaurant/model/restaurant_model.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
@@ -33,18 +34,29 @@ class RestaurantScreen extends StatelessWidget {
               return ListView.separated(
                   itemBuilder: (context, index) {
                     final item = snapshot.data![index];
+                    final pItem = RestaurantModel(
+                        id: item['id'],
+                        name: item['name'],
+                        thumbUrl: item['thumbUrl'],
+                        tags: List<String>.from(item['tags']),
+                        priceRange: RestaurantPriceRange.values
+                            .firstWhere((e) => e.name == item['priceRange']),
+                        ratings: item['ratings'],
+                        ratingsCount: item['ratingsCount'],
+                        deliveryTime: item['deliveryTime'],
+                        deliveryFee: item['deliveryFee']);
 
                     return RestaurantCard(
                       image: Image.network(
-                        '$ip${item['thumbUrl']}',
+                        '$ip${pItem.thumbUrl}',
                         fit: BoxFit.cover,
                       ),
-                      name: item['name'],
-                      tags: List<String>.from(item['tags']),
-                      ratingsCount: item['ratingsCount'],
-                      deliveryTime: item['deliveryTime'],
-                      deliveryFee: item['deliveryFee'],
-                      ratings: item['ratings'],
+                      name: pItem.name,
+                      tags: pItem.tags,
+                      ratingsCount: pItem.ratingsCount,
+                      deliveryTime: pItem.deliveryTime,
+                      deliveryFee: pItem.deliveryFee,
+                      ratings: pItem.ratings,
                     );
                   },
                   separatorBuilder: (context, index) {
