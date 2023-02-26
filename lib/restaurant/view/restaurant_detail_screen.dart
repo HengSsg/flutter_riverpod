05edd8d9271775102a7170389bdb07a45d0d5007
HengSsg <dnflekf2748@gmail.com>
2023-02-26 10:19:50 +0900

    SliverList 구현하기

--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -9,22 +9,54 @@ class RestaurantDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
-      title: '불타는 떡볶이',
-      child: Column(
-        children: [
-          RestaurantCard(
-            image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
-            name: '불타는 떡볶이',
-            tags: ['ㄴㅇㄹㅇㄴ', 'ㄴㅁㅇㄹㄴㅇ'],
-            ratingsCount: 100,
-            deliveryTime: 20,
-            deliveryFee: 2032,
-            ratings: 4.23,
-            isDetail: true,
-            detail: '맛있는 떡볶이',
-          ),
-          ProductCard()
-        ],
+        title: '불타는 떡볶이',
+        child: CustomScrollView(
+          slivers: [
+            renderTop(),
+            renderLabel(),
+            renderProducts(),
+          ],
+        ));
+  }
+
+  SliverPadding renderLabel() {
+    return const SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          '메뉴',
+          style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+        ),
+      ),
+    );
+  }
+
+  renderProducts() {
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate((context, index) {
+          return const Padding(
+            padding: EdgeInsets.only(top: 16.0),
+            child: ProductCard(),
+          );
+        }, childCount: 10),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter renderTop() {
+    return SliverToBoxAdapter(
+      child: RestaurantCard(
+        image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
+        name: '불타는 떡볶이',
+        tags: ['ㄴㅇㄹㅇㄴ', 'ㄴㅁㅇㄹㄴㅇ'],
+        ratingsCount: 100,
+        deliveryTime: 20,
+        deliveryFee: 2032,
+        ratings: 4.23,
+        isDetail: true,
+        detail: '맛있는 떡볶이',
       ),
     );
   }
