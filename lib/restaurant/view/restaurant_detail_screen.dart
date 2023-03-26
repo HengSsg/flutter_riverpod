324bbbe639d8b5d38578beef1c585f5a59e4e722
HengSsg <dnflekf2748@gmail.com>
2023-03-26 20:29:24 +0900

    RestaurantRepositoryProvider 작업하기

--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -17,22 +17,16 @@ class RestaurantDetailScreen extends ConsumerWidget {
     required this.id,
   }) : super(key: key);
 
-  Future<RestaurantDetailModel> getRestaurantDetail(WidgetRef ref) async {
-    final dio = ref.watch(dioProvider);
-
-    final repository = RestaurantRepository(dio, baseUrl: '$ip/restaurant');
-
-    return repository.getRestaurantDetail(id: id);
-  }
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return DefaultLayout(
         title: '불타는 떡볶이',
         child: FutureBuilder<RestaurantDetailModel>(
-          future: getRestaurantDetail(ref),
+          future: ref.watch(restaurantRepositoryProvider).getRestaurantDetail(
+                id: id,
+              ),
           builder: (context, AsyncSnapshot<RestaurantDetailModel> snapshot) {
-            if(snapshot.hasError){
+            if (snapshot.hasError) {
               return Center(
                 child: Text(snapshot.error.toString()),
               );
