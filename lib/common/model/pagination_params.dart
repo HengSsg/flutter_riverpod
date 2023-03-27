7e450618461215cdcae7aa49d6d4b03a1ebbf0d2
HengSsg <dnflekf2748@gmail.com>
2023-03-27 22:56:09 +0900

    Restaurant Pagiantion - 1

--- lib/common/model/pagination_params.dart
+++ lib/common/model/pagination_params.dart
@@ -14,4 +14,6 @@ class PaginationParams {
 
   factory PaginationParams.fromJson(Map<String, dynamic> json) =>
       _$PaginationParamsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PaginationParamsToJson(this);
 }
